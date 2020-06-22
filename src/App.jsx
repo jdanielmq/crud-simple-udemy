74230bfade17ada379d7813cafab6346a53a68bd
Juan Daniel Muñoz Queupul <jdannymq@gmail.com>
2020-06-22 13:17:00 -0400

    primer commit en capitutlo 38.- Formulario y Generador de IDS (shortid)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 
 function App() {
+
+  const [tarea, setTarea] = React.useState('');
+
+  const [tareas, setTareas] = React.useState([]);
+
+  const shortid = require('shortid');
+
+  const agregarTarea = e =>{
+    e.preventDefault()
+
+    if(!tarea.trim()){
+      console.log('Elemento tarea viene vacio');
+      return;
+    }
+
+    console.log(tarea);
+
+    setTareas([
+      ...tareas,
+      {tarea, id: shortid.generate()}
+    ]);
+
+    setTarea('');
+  }
+
+
   return (
     <div className="container">
       <h1 className="text-center">Crud Simple</h1>
@@ -18,6 +44,16 @@ function App() {
           </div>
           <div className="col-4">
             <h4 className="text-center">Formulario</h4>
+            <form onSubmit={ agregarTarea }>
+              <input 
+                type="text" 
+                className="form-control mb-2"
+                placeholder="Ingrese Tarea"
+                onChange={e => setTarea(e.target.value)}
+                value={ tarea }
+              />
+              <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+            </form>            
           </div>
       </div>
     </div>
